389cd6ebd8becf9a5fef9a5bd876f18ce06440b4
Scanner <scanner@apricot.com>
2022-01-12 11:14:21 -0800

    Make sure the number of user objects is the actual count instead of a hard coded number
    The test may not be using a cleared db when running and thus the hardcoded value was failing.
    XXX Worrisome.

--- mibudge/users/tests/test_tasks.py
+++ mibudge/users/tests/test_tasks.py
@@ -1,16 +1,20 @@
 import pytest
 from celery.result import EagerResult
+from django.contrib.auth import get_user_model
 
 from mibudge.users.tasks import get_users_count
 from mibudge.users.tests.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
 
+User = get_user_model()
+
 
 def test_user_count(settings):
     """A basic test to execute the get_users_count Celery task."""
     UserFactory.create_batch(3)
+    num_user_objects = User.objects.count()
     settings.CELERY_TASK_ALWAYS_EAGER = True
     task_result = get_users_count.delay()
     assert isinstance(task_result, EagerResult)
-    assert task_result.result == 3
+    assert task_result.result == num_user_objects
