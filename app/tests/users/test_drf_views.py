ff33b56c774a9b2c4484a3763da7ffa9e4b68adb
Scanner <scanner@apricot.com>
2026-05-21 22:49:35 -0700

    Missed fixing a test.

--- app/tests/users/test_drf_views.py
+++ app/tests/users/test_drf_views.py
@@ -75,6 +75,7 @@ class TestUserViewSet:
 
         assert response.data == {
             "username": user.username,
+            "email": user.email,
             "name": user.name,
             "url": f"http://testserver/api/v1/users/{user.username}/",
             "default_bank_account": None,
